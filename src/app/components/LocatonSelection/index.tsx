ee736cab63f433c64218040168bea94f98b8f365
wellitonaraujo <wellitonaraujodev@hotmail.com>
2023-10-21 12:27:24 -0300

    menu location init

--- src/app/components/LocatonSelection/index.tsx
+++ src/app/components/LocatonSelection/index.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+"use client";
+
+import { FaMapMarkerAlt } from "react-icons/fa";
+import { Menu } from "@headlessui/react";
+import React, { useState } from "react";
+
+const locations = [
+  "Main Street 123, Brasil",
+  "Main Street 32, Estados Unidos",
+  "Park Road 333, Reino Unido",
+];
 
 const LocatonSelection = () => {
-  return <div>LocatonSelection</div>;
+  const [location, setLocation] = useState("Selecione uma localização");
+
+  return (
+    <Menu as="div" className="w-full h-full flex xl:flex-row">
+      <div className="relative flex-1">
+        <Menu.Button className="dropdown-btn w-full h-full flex flex-col justify-center items-center xl:items-start xl:pl-8">
+          <div className="w-full h-16 xl:h-full flex justify-center xl:justify-start xl:border-r xl:border-black/10">
+            <div className="flex flex-col justify-center">
+              <div className="flex flex-col xl:flex-row items-center xl:gap-x-2 gap-y-2 xl:gap-y-0">
+                <FaMapMarkerAlt className="text-accent" />
+                Selecione uma localização
+              </div>
+              <div>{location}</div>
+            </div>
+          </div>
+        </Menu.Button>
+        <Menu.Items>ITEM</Menu.Items>
+      </div>
+    </Menu>
+  );
 };
 
 export default LocatonSelection;
--- src/app/components/SearchMobile/index.tsx
+++ src/app/components/SearchMobile/index.tsx
@@ -1,7 +1,29 @@
+import LocatonSelection from "../LocatonSelection";
+import HoursSelection from "../HoursSelection";
+import DateSelection from "../DateSelection";
+
 import React from "react";
 
 const SearchMobile = () => {
-  return <div>SearchMobile</div>;
+  return (
+    <div className="xl:hidden font-medium">
+      <div className="container mx-auto">
+        <div className="flex flex-col gap-y-4">
+          <LocatonSelection />
+
+          <DateSelection />
+
+          <HoursSelection />
+
+          <div className="flex items-center px-6">
+            <button className="btn btn-sm btn-accent w-[164px] mx-auto">
+              Buscar
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SearchMobile;
